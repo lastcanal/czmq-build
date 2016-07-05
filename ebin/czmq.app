5b58dd0c59aea5471bc9ea6605fddfc4f16c4cd3
Your Name <you@example.com>
2016-07-05 23:27:27 +0000

    czmq-build

---
+++ ebin/czmq.app
@@ -0,0 +1,9 @@
+{application,czmq,
+             [{description,"Erlang bindings for CZMQ"},
+              {vsn,"0.1.0"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {env,[]},
+              {modules,[czmq,czmq_benchmark,czmq_poller,czmq_reloader,
+                        czmq_test,erlzmq_benchmark,ezmq_benchmark,
+                        zmq_gen_benchmark]}]}.
---
+++ include/czmq.hrl
@@ -0,0 +1,18 @@
+-define(ZMQ_PAIR, 0).
+-define(ZMQ_PUB, 1).
+-define(ZMQ_SUB, 2).
+-define(ZMQ_REQ, 3).
+-define(ZMQ_REP, 4).
+-define(ZMQ_DEALER, 5).
+-define(ZMQ_ROUTER, 6).
+-define(ZMQ_PULL, 7).
+-define(ZMQ_PUSH, 8).
+-define(ZMQ_XPUB, 9).
+-define(ZMQ_XSUB, 10).
+-define(ZMQ_STREAM, 11).
+
+-define(ZFRAME_MORE, 1).
+-define(ZFRAME_REUSE, 2).
+-define(ZFRAME_DONTWAIT, 4).
+
+-define(CURVE_ALLOW_ANY, "*").
